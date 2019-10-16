d440800e4a7033ac1aca8342377ffac7eb1a2727
zilebe <zile93@hotmail.com>
2019-10-16 16:13:01 -0400

    Updates

---
+++ precache-manifest.85195cb851ef001f53110265eabdd946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1bee7f55b46f466e8058f48ed8bfd8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1f7adee7d03d63529068",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9c0ebd2f4cd4e858fd45",
+    "url": "/robofriends/static/css/main.fd20eac8.chunk.css"
+  },
+  {
+    "revision": "1f7adee7d03d63529068",
+    "url": "/robofriends/static/js/2.efde438a.chunk.js"
+  },
+  {
+    "revision": "9c0ebd2f4cd4e858fd45",
+    "url": "/robofriends/static/js/main.c21f05ac.chunk.js"
+  },
+  {
+    "revision": "8df7f21b05c730e9f3c7",
+    "url": "/robofriends/static/js/runtime-main.8151d04f.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zilebe/robofriends/precache-manifest.cde98e6418b95ebbc12df3102dab0d49.js"
+  "/robofriends/precache-manifest.85195cb851ef001f53110265eabdd946.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zilebe/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
