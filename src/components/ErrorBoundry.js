bd22bf30f59880c32bf641d732eea2e2b3575446
zilebe <zile93@hotmail.com>
2019-10-16 18:24:57 -0400

    texting

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -14,7 +14,7 @@ class ErrorBoundry extends Component {
 
 	render() {
 		if (this.state.hasError) {
-			return <h1>Ooops, something went wrong!</h1>;
+			return <h1>Ooops, something went wrong!!!</h1>;
 		}
 		return this.props.children;
 	}
