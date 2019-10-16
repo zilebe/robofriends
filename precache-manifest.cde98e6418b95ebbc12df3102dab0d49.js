4395c49bd532bb756fbbc85dc08d2959a17d2e56
zilebe <zile93@hotmail.com>
2019-10-16 16:03:00 -0400

    Updates

---
+++ precache-manifest.cde98e6418b95ebbc12df3102dab0d49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde572558262f40a02f12b16730c1cbe",
+    "url": "/zilebe/robofriends/index.html"
+  },
+  {
+    "revision": "1f7adee7d03d63529068",
+    "url": "/zilebe/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b480c44c40c778d7a8c0",
+    "url": "/zilebe/robofriends/static/css/main.e17f8396.chunk.css"
+  },
+  {
+    "revision": "1f7adee7d03d63529068",
+    "url": "/zilebe/robofriends/static/js/2.efde438a.chunk.js"
+  },
+  {
+    "revision": "b480c44c40c778d7a8c0",
+    "url": "/zilebe/robofriends/static/js/main.5efdfe24.chunk.js"
+  },
+  {
+    "revision": "65e3ef8388699aabbeef",
+    "url": "/zilebe/robofriends/static/js/runtime-main.de5446b6.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/zilebe/robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
